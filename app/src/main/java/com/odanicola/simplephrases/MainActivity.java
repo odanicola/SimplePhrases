706bdaa4ad958c2efa4e6bd17d1385d55b4bd110
Oda Aditiya Nicola <odanicola@gmail.com>
2019-01-22 09:58:13 +0700

    update adding function originally from Rob Percival

--- app/src/main/java/com/odanicola/simplephrases/MainActivity.java
+++ app/src/main/java/com/odanicola/simplephrases/MainActivity.java
@@ -37,6 +37,17 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void buttonTapped(View view){
+        int id = view.getId();
+        String ourId = "";
+        ourId = view.getResources().getResourceEntryName(id);
+
+        int resourceId = getResources().getIdentifier(ourId, "raw", getPackageName());
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, resourceId);
+        mediaPlayer.start();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
